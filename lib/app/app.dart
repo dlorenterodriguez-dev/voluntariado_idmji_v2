065e7ea7f6df1b78721d31c390cacfc2e1b68b0c
Dani <dlorenterodriguez@gmail.com>
2026-06-17 12:47:32 +0200

    test: actualizar prueba inicial de la aplicación

---
+++ lib/app/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class VolunteerApp extends StatelessWidget {
+  const VolunteerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Voluntariado IDMJI',
+      debugShowCheckedModeBanner: false,
+      home: const Scaffold(
+        body: Center(
+          child: Text('Voluntariado IDMJI'),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,6 @@
 import 'package:flutter/material.dart';
+import 'app/app.dart';
 
 void main() {
   runApp(const VolunteerApp());
-}
-
-class VolunteerApp extends StatelessWidget {
-  const VolunteerApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Voluntariado IDMJI',
-      debugShowCheckedModeBanner: false,
-      home: const Scaffold(
-        body: Center(
-          child: Text('Voluntariado IDMJI'),
-        ),
-      ),
-    );
-  }
 }
