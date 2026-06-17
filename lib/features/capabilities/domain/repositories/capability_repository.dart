1c0213262fe4119830094eef23ff726c3291d567
Dani <dlorenterodriguez@gmail.com>
2026-06-17 13:49:32 +0200

    refactor: adaptar CapabilityRepository a soft delete

--- lib/features/capabilities/domain/repositories/capability_repository.dart
+++ lib/features/capabilities/domain/repositories/capability_repository.dart
@@ -1,13 +1,13 @@
 import '../entities/capability.dart';
 
 abstract interface class CapabilityRepository {
-  Future<List<Capability>> getAll();
+  Future<List<Capability>> getAllActive();
+
+  Future<List<Capability>> getAllIncludingInactive();
 
   Future<Capability?> getById(String id);
 
   Future<void> create(Capability capability);
 
   Future<void> update(Capability capability);
-
-  Future<void> deleteById(String id);
 }
